6e4a038d1e91f6020c8571d1e616f4089311522f
ejsongs <thdmswl1218@gmail.com>
2020-02-11 14:17:33 +0900

    슬라이드 상게 기능 추가

--- res/js/index.js
+++ res/js/index.js
@@ -1,20 +1,36 @@
+startSlide();
+fillLoader();
+
+
 function startSlide() {
     var index = 0;
     var totalSlide = $('.slide-wrap > div').length;
 
+    var i = 0;
     var timer = setInterval(function() { //실행할 스크립트
+        fillLoader();
         if  (index == (totalSlide  -1)  ) {
             $('.slide-wrap').css('left',0);
             index= -1;
-            // console.log('find');
         }
-        $('.loader-wrap').css('visibility','visible'); 
         $(".slide-wrap").animate({
             left:'-'+100*(index+1)+'%'
         }, 1000 );
         index = index + 1;
-
-    },2000);
+    },5000);
 }
 
-startSlide();
+function fillLoader() {
+    setTimeout(function() {
+        $('#loadWrap').css('visibility','visible');
+        $(".fill").animate({
+            width:'100%',
+        }, 4000 , function(){
+            $('.fill').css('width', '0%');
+            $('#loadWrap').css('visibility','hidden');
+        });
+    }, 1000);
+}
+
+function rollingText() {
+}
